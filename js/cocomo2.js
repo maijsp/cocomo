11e0b1757a8c3e3ae33f9b37498c886b3216658d
iisomoh <komolmarch.tre@student.mahidol.ac.th>
2020-05-10 21:38:14 +0700

    17 driver
    -add 17 driver
    -but not finish for UI and all calculate

--- js/cocomo2.js
+++ js/cocomo2.js
@@ -68,3 +68,52 @@ function calculateEarlyDesign() {
     '   </h5></div></div></div>')
 }
 
+function calculateBPost()
+{
+    
+        var firstfact1 = document.getElementById('PREC')
+        var secondfact1 = document.getElementById('DFEX') 
+        var thirdfact1 = document.getElementById('ARCH')
+        var forthfact1 = document.getElementById('TEAM')
+        var fifthfact1 = document.getElementById('PRMY')
+        var one1 = firstfact1.options[firstfact1.selectedIndex].value
+        var second1 = secondfact1.options[secondfact1.selectedIndex].value
+        var three1 = thirdfact1.options[thirdfact1.selectedIndex].value
+        var forth1 = forthfact1.options[forthfact1.selectedIndex].value
+        var fifth1 = fifthfact1.options[fifthfact1.selectedIndex].value
+        var sum1 = 0
+        sum1 = parseFloat(one1) + parseFloat(second1) + parseFloat(three1) + parseFloat(forth1) + parseFloat(fifth1)
+        console.log(sum1)
+        var B1 = 1.01+(0.01 *(sum1));
+        console.log(B1)
+        return B1;
+}
+
+function calculatePost()
+{
+    var A1 = 2.5
+    var B1 = calculateBPost()
+    var RELY = parsefloat(document.getElementById('RELY').value)
+    var DATA = parsefloat(document.getElementById('DATA').value)
+    var CPLX = parsefloat(document.getElementById('CPLX').value)
+    var RUSE1 = parsefloat(document.getElementById('RUSE1').value)
+    var DOCU = parsefloat(document.getElementById('DOCU').value)
+    var TIME = parsefloat(document.getElementById('TIME').value)
+    var STOR = parsefloat(document.getElementById('STOR').value)
+    var PVOL = parsefloat(document.getElementById('PVOL').value)
+    var ACAP = parsefloat(document.getElementById('ACAP').value)
+    var PACP = parsefloat(document.getElementById('PCAP').value)
+    var PCON = parsefloat(document.getElementById('PCON').value)
+    var AEXP = parsefloat(document.getElementById('AEXP').value)
+    var PEXP = parsefloat(document.getElementById('PEXP').value)
+    var LTEX = parsefloat(document.getElementById('LTEX').value)
+    var TOOL = parsefloat(document.getElementById('TOOL').value)
+    var SITE = parsefloat(document.getElementById('SITE').value)
+    var SCED = parsefloat(document.getElementById('SCED').value)
+
+    var EM1 = RELY * DATA * CPLX * RUSE1 * DOCU * TIME * STOR * PVOL * ACAP * PACP * PCON * AEXP 
+             * PEXP * LTEX * TOOL * SITE * SCED
+    //console.log(EM1)
+        
+}
+
