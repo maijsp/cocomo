bb7e798ca2fec722059aa3fb5efbabd25124104b
iisomoh <komolmarch.tre@student.mahidol.ac.th>
2020-05-12 23:22:51 +0700

    create coqualmo.js
    di driver

---
+++ js/coqualmo.js
@@ -0,0 +1,6 @@
+function calculateReq()
+{
+    var ACAP =  document.querySelector('input[name="req-acap"]:checked').value
+    var PCAP =  document.querySelector('input[name="req-pcap"]:checked').value
+    var AEXP = document.querySelector('input[name="req-aexp"]:checked').value
+}
